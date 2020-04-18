b030222da1957648b9dff18963147b9220845813
Ryan Ofori <ryanofori1@gmail.com>
2020-04-18 04:13:18 -0400

    init get post request

---
+++ NetworkLayer/NetworkManager.swift
@@ -0,0 +1,67 @@
+//
+//  NetworkManager.swift
+//  NetworkLayer
+//
+//  Created by Ryan Ofori on 4/18/20.
+//  Copyright © 2020 Ryan Ofori. All rights reserved.
+//
+
+import UIKit
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    func getData(urlString: String, completion: @escaping(Data) -> Void) {
+        guard let url = URL(string: urlString) else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        let task = URLSession.shared.dataTask(with: request) {data, response, error in
+            guard let data = data else { return }
+            completion(data)
+            if let response = response {
+                NSLog(response.description)
+            }
+            if let error = error {
+                NSLog(error.localizedDescription)
+            }
+        }
+        task.resume()
+    }
+    
+    func postData(urlString: String, param: String, completion: @escaping([String: Any]) -> Void) {
+        let param: String = param
+        let data = param.data(using: .utf8)
+        guard let url = URL(string: urlString) else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        request.httpBody = data
+        let task = URLSession.shared.dataTask(with: request, completionHandler: {data, response, error  in
+            if let response = response {
+                NSLog(response.description)
+            }
+            if let error = error {
+                NSLog(error.localizedDescription)
+            }
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+                    completion(json ?? ["": (Any).self])
+                } catch {
+                    NSLog(error.localizedDescription)
+                }
+            }
+        })
+        task.resume()
+    }
+    
+    func deleteMethod() {
+        
+    }
+    
+    func putMethod() {
+        
+    }
+}
