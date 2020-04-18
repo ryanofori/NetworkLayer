6895173c381568059aa2431d70a7253913a94488
Ryan Ofori <ryanofori1@gmail.com>
2020-04-18 03:52:41 -0400

    first commit

---
+++ NetworkLayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkLayer
+//
+//  Created by Ryan Ofori on 4/18/20.
+//  Copyright © 2020 Ryan Ofori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
