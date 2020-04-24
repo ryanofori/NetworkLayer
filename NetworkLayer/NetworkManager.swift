f256159c79d5c548caaafb05f0e0237be79ce964
Ryan Ofori <ryanofori1@gmail.com>
2020-04-24 12:29:28 -0400

    Trying to make a get test and add upload and downloadTask for network manager

--- NetworkLayer/NetworkManager.swift
+++ NetworkLayer/NetworkManager.swift
@@ -12,97 +12,109 @@ class NetworkManager {
     static let shared = NetworkManager()
     
     private init() {}
+    
+    func networkURL(url: String) {
+        guard let url = URL(string: url) else { NetworkError.staus.invalidURL
+            return }
+    }
+    
+    
     //3 methods max
     //param [String: String]
     func requestMethod(url: String, param: String = "", httpMethod: String = "GET", bodyValue:String = "", headerField: String = "", completion: @escaping(Data?, URLResponse? ,Error?) -> Void) {
         let param: String = param
         let data = param.data(using: .utf8)
-        guard let url = URL(string: url) else { NetworkError.staus.invalidURL; return }
+        guard let url = URL(string: url) else { NetworkError.staus.invalidURL
+            return }
         var request = URLRequest(url: url)
         request.httpBody = data
-        
         if httpMethod == "POST" {
             request.setValue(bodyValue, forHTTPHeaderField: headerField)
         }
         request.httpMethod = httpMethod
-
         let task = URLSession.shared.dataTask(with: request) {data, response, error in
             completion(data, response, error)
-                }
-                task.resume()
-        
-        
-    }
-    
-    func dataTask(request: URLRequest) {
-        let task = URLSession.shared.dataTask(with: request) {data, response, error in
-            
-            //completion(data, response, error)
         }
         task.resume()
     }
     
-    func downTask(request: URLRequest) {
-            let downloadTask = URLSession.shared.downloadTask(with: request) {data, response, error in
-  
-    //completion(data, response, error)
-               
-            }
-            downloadTask.resume()
-        
+    func dataTask(request: URLRequest, completion: @escaping(Data?, URLResponse?, Error?) -> Void) {
+        let task = URLSession.shared.dataTask(with: request) {data, response, error in
+            completion(data, response, error)
         }
+        task.resume()
+    }
     
-    func uploadTask(request: URLRequest) {
-            let uploadTask = URLSession.shared.downloadTask(with: request) {data, response, error in
-    //completion(data, response, error)
-            }
-            uploadTask.resume()
+    func downTask(urlString: String) {
+        guard let url = URL(string: urlString) else { NetworkError.staus.invalidURL
+            return
         }
+        let downloadTask = URLSession.shared.downloadTask(with: url) { (url, response, error) in
+            if let localURL = url {
+                if let string = try? String(contentsOf: localURL) {
+                    print(string)
+                }
+            }
+        }
+
+        downloadTask.resume()
+    }
     
+    func uploadTask(request: URLRequest, bodyData: Data) {
+        let uploadTask = URLSession.shared.uploadTask(with: request, from: bodyData)
+        uploadTask.resume()
+    }
     
+//    func uploadTask(request: URLRequest, completion: @escaping(Data?, URLResponse? ,Error?) -> Void) {
+//
+//        let uploadTask = URLSession.shared.downloadTask(with: request) { data, response, error in
+//            completion(data, response, error)
+//        }
+//        uploadTask.resume()
+//    }
     
     func getData(urlString: String, completion: @escaping(Data) -> Void) {
-            guard let url = URL(string: urlString) else { return }
-            var request = URLRequest(url: url)
-            request.httpMethod = "GET"
-            let task = URLSession.shared.dataTask(with: request) {data, response, error in
-                guard let data = data else { return }
-                completion(data)
-    //            if let response = response {
-    //                NSLog(response.description)
-    //            }
-                if let error = error {
+        guard let url = URL(string: urlString) else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        let task = URLSession.shared.dataTask(with: request) {data, response, error in
+            guard let data = data else { return }
+            completion(data)
+            //            if let response = response {
+            //                NSLog(response.description)
+            //            }
+            if let error = error {
+                NSLog(error.localizedDescription)
+            }
+        }
+        task.resume()
+    }
+    
+    func postData(urlString: String, param: String, completion: @escaping([String: Any]) -> Void) {
+        let param: String = param
+        let data = param.data(using: .utf8)
+        guard let url = URL(string: urlString) else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        request.httpBody = data
+        let task = URLSession.shared.dataTask(with: request, completionHandler: {data, response, error  in
+            if let response = response {
+                NSLog(response.description)
+            }
+            if let error = error {
+                NSLog(error.localizedDescription)
+            }
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+                    completion(json ?? ["": (Any).self])
+                } catch {
                     NSLog(error.localizedDescription)
                 }
             }
-            task.resume()
-        }
-        
-        func postData(urlString: String, param: String, completion: @escaping([String: Any]) -> Void) {
-            let param: String = param
-            let data = param.data(using: .utf8)
-            guard let url = URL(string: urlString) else { return }
-            var request = URLRequest(url: url)
-            request.httpMethod = "POST"
-            request.setValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
-            request.httpBody = data
-            let task = URLSession.shared.dataTask(with: request, completionHandler: {data, response, error  in
-                if let response = response {
-                    NSLog(response.description)
-                }
-                if let error = error {
-                    NSLog(error.localizedDescription)
-                }
-                if let data = data {
-                    do {
-                        let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
-                        completion(json ?? ["": (Any).self])
-                    } catch {
-                        NSLog(error.localizedDescription)
-                    }
-                }
-            })
-            task.resume()
-        }
+        })
+        task.resume()
+    }
     
 }
--- NetworkLayerTests/NetworkLayerTests.swift
+++ NetworkLayerTests/NetworkLayerTests.swift
@@ -11,24 +11,40 @@ import XCTest
 
 class NetworkLayerTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+//    override func setUpWithError() throws {
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    }
+//
+//    override func tearDownWithError() throws {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    }
+    
+    func testGet() {
+        let manage = NetworkManager.shared
+        let url = "https://rss.itunes.apple.com/api/v1/us/apple-music/top-albums/all/100/explicit.json"
+        let invaidURL = "ksdjfhl"
+//        manage.getData(urlString: invaidURL) { (data) in
+//            let mockData = String(decoding: data, as: UTF8.self)
+//        }
+        manage.getData(urlString: url) { (data) in
+            if let jsonString = String(data: data, encoding: .utf8){
+                
+            
+            }
         }
+
     }
+
+//    func testExample() throws {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//    }
+//
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
